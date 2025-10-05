f899ecbbfa2b9711da1db02c67a17c2665398f54
Abhishek <Abhishekraut8697@gmail.com>
2025-10-05 16:29:00 +0530

    configured contest routes

---
+++ apps/backend/src/routes/contest.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from "express"
+import {
+  getActiveChallenges,
+  getFinishedChallenges,
+  getContestChallenges,
+  submitContest,
+  getSpecificChallenge,
+  submitChallenge,
+  getContestLeaderboard
+} from "../controllers/contest.controller"
+
+const router: Router = Router()
+
+// Return all the active challenges 
+router.route('/active').get(getActiveChallenges)
+
+// Return all the finished challenges
+router.route('/finished').get(getFinishedChallenges)
+
+// Return all sub challenges and their start timings
+router.route('/:contestId').get(getContestChallenges)
+
+// Submit the contest with number of problems/challenge solved
+router.route('/submit/:contestId').post(submitContest)
+
+// Return the specific challenge 
+router.route('/:contestId/:challengeId').get(getSpecificChallenge)
+
+// Submit a specific challenge
+router.route('/submit/:challengeId').post(submitChallenge)
+
+// Return the leaderboard from db if the contest is end otherwise from sorted sets
+router.route('/leaderboard/:contestId').get(getContestLeaderboard)
+
+export default router;
