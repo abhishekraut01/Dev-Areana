9184f3fa54a7619db44f3224e1ead66ea57ae2f9
Abhishek <Abhishekraut8697@gmail.com>
2025-10-06 23:25:29 +0530

    fix:apiResponse

--- packages/utils/src/backendUtils/apiResponse.ts
+++ packages/utils/src/backendUtils/apiResponse.ts
@@ -2,14 +2,14 @@ class ApiResponse<T> {
     public success: boolean;
     public statusCode: number;
     public message: string;
-    public data?: T; 
-  
+    public data?: T;
+
     constructor(statusCode: number, message: string, data?: T) {
-      this.success = statusCode >= 200; 
-      this.statusCode = statusCode;
-      this.message = message;
-      this.data = data;
+        this.success = statusCode >= 200 && statusCode < 300;
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
     }
-  }
-  
-  export default ApiResponse;
+}
+
+export default ApiResponse;
