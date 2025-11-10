911961f13dd92f458c9ffe5a9004547d68984f88
Abhishek <Abhishekraut8697@gmail.com>
2025-11-10 21:16:37 +0530

    setup express app

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import errorHandler from "./middleware/globalErrorHandler.middleware";
+import cookieParser from 'cookie-parser'
+import { ApiError } from "@repo/utils";
+const app:Application = express();
+
+app.use(cookieParser());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+
+
+// Import and use routes
+import authRoute from './routes/auth.routes';
+app.use('/api/v1/auth', authRoute);
+
+
+// 404 Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new ApiError(404, 'Route Not Found'));
+});
+
+app.use(errorHandler)
+export default app
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,6 @@
-import express from "express";
-
-const app = express();
-
+import app from "./app";
+import dotenv from 'dotenv'
+dotenv.config()
 app.listen(process.env.PORT ?? 5000, () => {
   console.log(`server is running on port : ${process.env.PORT ?? 5000}`);
 });
